0ea2e9ea0485442c4436651384ca11e8efe72672
0927014051 <sontrinh2507@gmail.com>
2024-03-22 18:19:07 +0700

    commit
    commit

--- HighLandCoffeeApi/src/main/java/com/javaweb/SpringbootWebApplication.java
+++ HighLandCoffeeApi/src/main/java/com/javaweb/SpringbootWebApplication.java
@@ -9,10 +9,18 @@ import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDeta
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootWebApplication.class, args);
 	}
+	@GetMapping("/message")
+	public String messgae() {
+		return "deploy success with azure ";
+	}
+	
 }
