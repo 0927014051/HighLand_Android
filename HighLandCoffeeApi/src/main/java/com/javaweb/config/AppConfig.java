643ad52c1a175c197d1ecddbaa290b9e3eb2e475
0927014051 <thanhson12a12002@gmail.com>
2024-04-25 01:02:32 +0700

    add link for customer

--- HighLandCoffeeApi/src/main/java/com/javaweb/config/AppConfig.java
+++ HighLandCoffeeApi/src/main/java/com/javaweb/config/AppConfig.java
@@ -35,6 +35,7 @@ public class AppConfig extends WebSecurityConfigurerAdapter {
             .and()
             .authorizeRequests()
             .antMatchers("/api/admin/product/find/{productId}").hasAnyAuthority("CUSTOMER","STAFF","ADMIN")
+            .antMatchers("/api/admin/order/{order_id}/find").hasAnyAuthority("CUSTOMER","STAFF","ADMIN")
                 .antMatchers("/api/admin/**").hasAnyAuthority("ADMIN","STAFF")
                 .antMatchers("/api/users/find").hasAnyAuthority("STAFF","ADMIN","CUSTOMER")
                 .antMatchers("/api/users/all").hasAnyAuthority("STAFF","ADMIN","CUSTOMER")
